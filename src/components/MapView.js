d21cdd6cd2e5f733e0d5a405dfa528ca0fe249bf
Bearbrice <berclazpro@gmail.com>
2020-10-08 10:53:53 +0200

    Marker and popup

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,25 +1,29 @@
-import React, { Component } from 'react';
-import { Map, TileLayer } from 'react-leaflet';
+import React, {Component} from 'react';
+import {Map, TileLayer} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import data from '../assets/data';
+import Markers from './VenueMarkers';
 
 class MapView extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentLocation: { lat: 46.3, lng: 7.5333 },
+            currentLocation: {lat: 46.3, lng: 7.5333},
             zoom: 12,
         }
     }
+
     render() {
-        const { currentLocation, zoom } = this.state;
+        const {currentLocation, zoom} = this.state;
         return (
             <Map center={currentLocation} zoom={zoom}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                />
+                /> <Markers venues={data.venues}/>
             </Map>
         );
     }
 }
+
 export default MapView;
